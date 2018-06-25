140207048a9946b2a593e8d6d42eec90563d1a05
Tomas Votava <tomas.votava@bizztreat.com>
2018-06-25 08:20:16 +0200

    submodules files initialisation

---
+++ storage/storage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  storage.py
+#  
+#  Copyright 2018 Tomáš Votava <info@tomasvotava.eu>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+if __name__ == '__main__':
+	print("This module is not meant to be run directly.")
+	import sys
+	sys.exit(0)
+
